106cbba9f205c1b7890d58c1619d651ef186929e
Enrico Dondero <enrico.dondero.p@gmail.com>
2024-03-07 12:03:26 +0100

    added source and header files

---
+++ ex2_1.X/main.c
@@ -0,0 +1,13 @@
+/*
+ * File:   main.c
+ * Author: enric
+ *
+ * Created on March 7, 2024, 12:02 PM
+ */
+
+
+#include "xc.h"
+
+int main(void) {
+    return 0;
+}
---
+++ ex2_1.X/timer.c
@@ -0,0 +1,13 @@
+/*
+ * File:   timer.c
+ * Author: enric
+ *
+ * Created on March 7, 2024, 12:00 PM
+ */
+
+
+#include "xc.h"
+
+int main(void) {
+    return 0;
+}
