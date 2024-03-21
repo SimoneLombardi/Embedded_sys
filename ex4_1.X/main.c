da722ae622c0e60ca39cc914f908581aa56e784a
Enrico Dondero <enrico.dondero.p@gmail.com>
2024-03-21 16:45:17 +0100

    partial implementation of UART1 initialization

--- ex4_1.X/main.c
+++ ex4_1.X/main.c
@@ -9,5 +9,15 @@
 #include "xc.h"
 
 int main(void) {
+    //pin mapping
+    //mapping of remappable pin RP64 on pin RD0 to work as UART1TX
+    RPOR0bits.RP64R=1; 
+    //mapping of remappable pin RPI75 on pin RD11 to work as UA1RX
+    RPINR18bits.U1RXR=0x4b; // 0x4b is 75 in hexadecimal
+    //UART1 init
+    //BaudRate selection
+    U1BRG=468; //(72MHz/(16*9600))-1
+    U1MODEbits.UARTEN=1; //global UART enable
+    U1STAbits.UTXEN=1; //UART1 enable
     return 0;
 }
