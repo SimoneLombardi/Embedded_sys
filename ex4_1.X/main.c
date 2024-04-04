edb4c34f44dbbf1864bc3003f643ba10a77ce2cf
Enrico Dondero <enrico.dondero.p@gmail.com>
2024-04-04 17:14:39 +0200

    implemented correctly exercise 4.1

--- ex4_1.X/main.c
+++ ex4_1.X/main.c
@@ -8,6 +8,16 @@
 
 #include "xc.h"
 
+void __attribute__((__interrupt__, no_auto_psv__))_U1RXInterrupt(){
+    char carattere;
+    IFS0bits.U1RXIF=0;
+    carattere=U1RXREG;
+    U1TXREG=carattere;
+    LATAbits.LATA0 = (!LATAbits.LATA0);
+    //Flag raised each time a character is received 
+    U1STAbits.URXISEL=0;
+}
+
 int main(void) {
     //pin mapping
     //mapping of remappable pin RP64 on pin RD0 to work as UART1TX
@@ -19,5 +29,11 @@ int main(void) {
     U1BRG=468; //(72MHz/(16*9600))-1
     U1MODEbits.UARTEN=1; //global UART enable
     U1STAbits.UTXEN=1; //UART1 enable
+    IEC0bits.U1RXIE=1; //UART1 interrupt enable
+    //led for debugging
+    TRISAbits.TRISA0 = 0; 
+    while(1){
+
+    }
     return 0;
 }
